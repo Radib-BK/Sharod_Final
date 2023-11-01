96fa3dacfb5b6f483382e30dcd477f1bdd0c024c
Radib <radib@iut-dhaka.edu>
2023-11-01 14:52:01 +0600

    Update blog.php

--- blog.php
+++ blog.php
@@ -12,7 +12,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title>SHAROD - Furniture & Interior Decoration</title>
+    <title>Blog - SHAROD</title>
     
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
